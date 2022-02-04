e19b656249cc97490a5d5b0d7b4a546cb32c5529
Vicky Das <workmailvicky101@gmail.com>
2022-02-04 22:04:52 +0530

    --Added info about positive and negative infinity

---
+++ Language_Fundamentals/infinityAndNegativeInfinity.js
@@ -0,0 +1,36 @@
+//Infinity and Negative infinity
+
+//infitinity is the  hightest possible value in js
+//Are of two types infinity and negative infinity
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY)
+
+//is js numbers are hold at 64-bit format
+//so if a number if larger than 64-bit highest possible number is will be either of positive infinity or negative infinity
+
+
+//Feature of js
+console.log(4e3==4000)//Here 4e3 is equal to 4000 
+
+//Maximum available value
+console.log(Number.MAX_VALUE)
+console.log(-Number.MAX_VALUE)
+
+//How you will put a validation for positive or negative infinity
+
+// if(num===Number.POSITIVE_INFINITY){
+//     return "Positive infinite value"
+// }else{
+//     return num//some calculation
+// }
+
+
+// if(num===Number.NEGATIVE_INFINITY){
+//     return "Negative infinite value"
+// }else{
+//     return num//some calculation
+// }
+
+
+// What will be the output of this code 
+// console.log(1/0)
